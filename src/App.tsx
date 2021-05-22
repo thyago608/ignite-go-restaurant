137f3791f1ad49b81b20cd9fae8ceeb68b02347b
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2021-05-22 10:43:10 -0400

    Criaçao do Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Routes from './routes';
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
